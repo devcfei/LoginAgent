856a3fa8fa6e87fdfcff69917cbdc666ee9dbded
Chao Fei <devcfei@outlook.com>
2024-09-01 15:23:01 +0800

    Enable account login

---
+++ LoginAgent/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by LoginAgent.rc
+//
+#define IDD_MAIN                        101
+#define IDI_FAVICON                     103
+#define IDC_CHECK_SERVER_MAINTAINANCE   1002
+#define IDC_EDIT_LOG                    1003
+#define IDC_BTN_RELOAD                  1004
+#define IDC_STATIC_PORT                 1009
+#define IDC_STATIC_USERCOUNT            1010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
